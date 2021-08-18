a9e36145faa9ac5cdde68a40e7afb8340ac0c854
rivalcoba <ivan.rivalcoba@gmail.com>
2021-08-18 14:52:18 -0500

    adding name to TheHeader

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,12 +1,17 @@
 <template>
   <header class="text-center bg-blue-800 text-white p-4 mb-10">
     <div class="text-3xl md:text-5xl font-bold mb-3">Covid-19 Tracker</div>
-    <p>APY by <a href="https://covid19api.com" class="text-blue-300">covid19api.com</a></p>
+    <p>
+      APY by
+      <a href="https://covid19api.com" class="text-blue-300">covid19api.com</a>
+    </p>
   </header>
 </template>
 
 <script>
-export default {};
+export default {
+  name: 'TheHeader',
+};
 </script>
 
 <style>
