7b22589feb8586c7a964536f7d8c1ecf407a8bfb
rivalcoba <ivan.rivalcoba@gmail.com>
2021-08-18 19:33:07 -0500

    Video Tag T21:52

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,39 @@
 <template>
-  <div class="home">
-    Hello World
-  </div>
+  <main v-show="!loading">Show Data</main>
+  <main class="flex flex-col align-center justify-center text-center" v-show="loading">
+    <div class="text-gray-500 text-3xl mt-10 mb-6">Fetching Data</div>
+    <img :src="loadingImage" class="w-24 m-auto" alt="loading" />
+  </main>
 </template>
 
 <script>
 export default {
   name: 'Home',
   components: {},
+  data() {
+    return {
+      loading: true,
+      title: 'Global',
+      dataDate: '',
+      stats: {},
+      countries: [],
+      // eslint-disable-next-line global-require
+      loadingImage: require('@/assets/loadingRing.gif'),
+    };
+  },
+  methods: {
+    async fetchCovidData() {
+      const res = await fetch('https://api.covid19api.com/summary');
+      const data = await res.json();
+      return data;
+    },
+  },
+  async created() {
+    const data = await this.fetchCovidData();
+    this.dataDate = data.Date;
+    this.stats = data.Global;
+    this.countries = data.Countries;
+    this.loading = false;
+  },
 };
 </script>
